ab0fdd62bc04d36d78c1a5545eee54634cfb1bfe
chlohal <email@clh.sh>
2024-03-18 11:27:34 -0400

    Improve drag/drop UX and decrease size of requests

--- src/src/static/scripts/editor.js
+++ src/src/static/scripts/editor.js
@@ -1,4 +1,4 @@
-const BLOCK_INFO_MAP = {}
+const BLOCK_INFO_MAP = {};
 
 async function main() {
     let my_id = location.pathname.split("/")[2];
@@ -21,22 +21,22 @@ async function create_new_block_buttons(editCanvas) {
     buttons.textContent = "";
 
     const req = await fetch(`/api/bucket/blocks`);
-    const data = (await req.json()).data;
+    const data = await req.json();
 
     const cats_butts = [];
 
-    for(const category of data) {
+    for (const category of data) {
         const category_button = make_category_button(category.name, categories);
         const cat_butts = document.createElement("div");
         cats_butts.push(cat_butts);
         buttons.appendChild(cat_butts);
 
         category_button.addEventListener("click", () => {
-            cats_butts.forEach(x=> x.style.display = "none");
+            cats_butts.forEach((x) => (x.style.display = "none"));
             cat_butts.style.display = "block";
         });
-        
-        for(const block of category.blocks) {
+
+        for (const block of category.blocks) {
             BLOCK_INFO_MAP[block.id] = block;
 
             block.color = category.color;
@@ -44,9 +44,8 @@ async function create_new_block_buttons(editCanvas) {
         }
     }
 
-    cats_butts.forEach(x=> x.style.display = "none");
+    cats_butts.forEach((x) => (x.style.display = "none"));
     cats_butts[0].style.display = "block";
-
 }
 
 function add_create_block_button(blockdef, parent, editCanvas) {
@@ -54,8 +53,8 @@ function add_create_block_button(blockdef, parent, editCanvas) {
     button.textContent = blockdef.name;
     button.style.backgroundColor = blockdef.color;
 
-    button.addEventListener("click", function() {
-        editCanvas.addBlock(blockdef)
+    button.addEventListener("click", function () {
+        editCanvas.addBlock(blockdef);
     });
 
     parent.appendChild(button);
@@ -81,21 +80,21 @@ fetch("/api/bucket/blocks", {
             blocks: [
                 {
                     name: "Attendance",
-                    id: "canvas.attendance"
+                    id: "canvas.attendance",
                 },
                 {
                     name: "List Students",
-                    id: "canvas.students"
+                    id: "canvas.students",
                 },
                 {
                     name: "Submit Assignment",
-                    id: "canvas.submit"
+                    id: "canvas.submit",
                 },
                 {
                     name: "Check Grades",
-                    id: "canvas.get_grades"
+                    id: "canvas.get_grades",
                 },
-            ]
+            ],
         },
         {
             name: "Users",
@@ -103,9 +102,9 @@ fetch("/api/bucket/blocks", {
             blocks: [
                 {
                     name: "QR Code Scan",
-                    id: "user.qrcode_scan"
+                    id: "user.qrcode_scan",
                 },
-            ]
+            ],
         },
         {
             name: "Logic",
@@ -113,9 +112,9 @@ fetch("/api/bucket/blocks", {
             blocks: [
                 {
                     name: "If",
-                    id: "logic.if"
+                    id: "logic.if",
                 },
-            ]
+            ],
         },
         {
             name: "Integrations",
@@ -123,62 +122,122 @@ fetch("/api/bucket/blocks", {
             blocks: [
                 {
                     name: "Clark Login",
-                    id: "integrate.login.sso.clark"
-                }
-            ]
-        }
-    ])
-})
+                    id: "integrate.login.sso.clark",
+                },
+            ],
+        },
+    ]),
+});
 
 async function create_edit_canvas(id, canvas) {
     const req = await fetch(`/api/bucket/${id}`);
-    const data = (await req.json()).data || {};
+    const data = (await req.json()) || {};
 
-    if(!data.blocks) data.blocks = []; //TEMP FOR DEBUG
+    if (!data.perspective) data.perspective = { x: 0, y: 0 };
+    if (!data.blocks) data.blocks = {}; //TEMP FOR DEBUG
 
     const canvasInner = document.createElement("div");
 
-    addDragging(canvasInner, (x,y) => {
-        data.perspective = {x, y};
-        sync();
-    }, data.perspective);
-    
+    addDragging(
+        canvasInner,
+        (x, y) => {
+            data.perspective = { x, y };
+            sync_perspective();
+        },
+        {
+            x: data.perspective.x,
+            y: data.perspective.y,
+            control_elem: canvas,
+        }
+    );
+
     canvas.appendChild(canvasInner);
 
-    function sync() {
-        fetch(`/api/bucket/${id}`, {
-            method: "PUT",
-            body: JSON.stringify(data)
-        })
+    function sync_perspective() {
+        fetch(`/api/bucket/${id}/perspective`, {
+            method: "PATCH",
+            body: JSON.stringify(data.perspective),
+        });
     }
 
-    for (const block of data.blocks) {
-        create_onscreen_block(block, sync, canvasInner);
+    function sync_block(block_id) {
+        fetch(`/api/bucket/${id}/blocks.${block_id}`, {
+            method: "PATCH",
+            body: JSON.stringify(data.blocks[block_id]),
+        });
+    }
+
+    function check_block_onscreen_sync(id) {
+        return function (block_elem) {
+            let block = data.blocks[id];
+            if (!block) return;
+
+            let is_still_onscreen = data.perspective.x + block.x >= 0;
+            if (is_still_onscreen) {
+                sync_block(id);
+            } else {
+                block_elem.remove();
+                delete data.blocks[id];
+                sync_block(id);
+            }
+        };
+    }
+
+    for (const [id, block] of Object.entries(data.blocks)) {
+        create_onscreen_block(
+            block,
+            check_block_onscreen_sync(id),
+            canvasInner
+        );
     }
 
     return {
-        addBlock: function(blockdef) {
+        addBlock: function (blockdef) {
+            const id = (Date.now() + Math.random())
+                .toString(36)
+                .replace(".", "_");
+
             const block = {
                 type: blockdef.id,
-                x: 0,
-                y: 0
+                x: data.perspective.x,
+                y: data.perspective.y,
             };
-            create_onscreen_block(block, sync, canvasInner);
-            data.blocks.push(block);
-            sync();
-        }
-    }
+            create_onscreen_block(
+                block,
+                check_block_onscreen_sync(id),
+                canvasInner
+            );
+            data.blocks[id] = block;
+            sync_block(id);
+        },
+    };
 }
 
 function create_onscreen_block(block_info, sync_callback, parent) {
-    const block_parent =document.createElement("div");
+    const block_parent = document.createElement("div");
     block_parent.classList.add("block-parent");
 
-    addDragging(block_parent, (x,y) => {
-        block_info.x = x;
-        block_info.y = y;
-        sync_callback()
-    }, {x: block_info.x, y: block_info.y});
+    let self = {};
+    self.remove = () => parent.removeChild(block_parent);
+
+    addDragging(
+        block_parent,
+        (x, y) => {
+            block_info.x = x;
+            block_info.y = y;
+            sync_callback(self);
+        },
+        {
+            x: block_info.x,
+            y: block_info.y,
+            start_dragging: () => {
+                parent.style.zIndex = 2;
+            },
+            stop_dragging: () => {
+                parent.style.zIndex = 0;
+            },
+        }
+    );
 
     const blockdef = BLOCK_INFO_MAP[block_info.type];
 
@@ -199,8 +258,8 @@ function create_onscreen_block(block_info, sync_callback, parent) {
  *
  * @param {HTMLDivElement} element
  */
-function addDragging(element, cb, {}) {
-    if(!pos) pos = {};
+function addDragging(element, cb, pos) {
+    if (!pos) pos = {};
 
     let dragging = false,
         originalX = 0,
@@ -210,34 +269,49 @@ function addDragging(element, cb, {}) {
 
     element.style.transform = `translate(${x}px, ${y}px)`;
 
-    element.addEventListener("mousedown", (e) => {
-        dragging = true;
-        originalX = e.clientX - x;
-        originalY = e.clientY - y;
-        
-        e.preventDefault();
-        e.stopPropagation();
-    });
-    element.addEventListener("mouseleave", (e) => {
+    function mouseleave(e) {
         dragging = false;
-    });
-    element.addEventListener("mousemove", (e) => {
+        if (cb) cb(x, y);
+    }
+
+    function mousemove(e) {
         if (dragging) {
-            x = (e.clientX - originalX);
-            y = (e.clientY - originalY);
+            x = e.clientX - originalX;
+            y = e.clientY - originalY;
 
             element.style.transform = `translate(${x}px, ${y}px)`;
         }
         e.preventDefault();
         e.stopPropagation();
-    });
+    }
 
-    element.addEventListener("mouseup", e => {
+    function mouseup(e) {
         dragging = false;
+        if(pos.stop_dragging) pos.stop_dragging();
+        
         e.preventDefault();
         e.stopPropagation();
 
-        if(cb) cb(x, y)
+        window.removeEventListener("mouseup", mouseup);
+        window.removeEventListener("mouseleave", mouseleave);
+        window.removeEventListener("mousemove", mousemove);
+
+        if (cb) cb(x, y);
+    }
+
+    (pos.control_elem || element).addEventListener("mousedown", (e) => {
+        dragging = true;
+        originalX = e.clientX - x;
+        originalY = e.clientY - y;
+
+        if(pos.start_dragging) pos.start_dragging();
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        window.addEventListener("mouseup", mouseup);
+        window.addEventListener("mouseleave", mouseleave);
+        window.addEventListener("mousemove", mousemove);
     });
 }
 
